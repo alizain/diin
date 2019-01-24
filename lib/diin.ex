99a56f262c015243afe4afa5294e959bd5cf02d7
Alizain Feerasta <alizain.feerasta@gmail.com>
2019-01-23 20:57:47 -0500

    Implementation

--- lib/diin.ex
+++ lib/diin.ex
@@ -1,18 +1,63 @@
 defmodule Diin do
-  @moduledoc """
-  Documentation for Diin.
-  """
+  @key :deps
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Diin.hello()
-      :world
-
+  Uses first_write_wins strategy for merging opts
   """
-  def hello do
-    :world
+  def parse(opts, config) do
+    opts
+    |> Keyword.get_values(@key)
+    |> Enum.reduce(%{}, fn
+      curr_deps, all_deps when is_map(curr_deps) ->
+        Map.merge(curr_deps, all_deps)
+
+      curr_deps, _all_deps ->
+        raise "#{inspect(@key)} must contain maps, given #{inspect(curr_deps)}"
+    end)
+    |> case do
+      all_deps when all_deps == %{} ->
+        config
+
+      all_deps ->
+        for {key, default_dep} <- config, into: %{} do
+          case Map.fetch(all_deps, key) do
+            {:ok, new_dep} ->
+              case validate_dep(default_dep, new_dep) do
+                :ok ->
+                  {key, new_dep}
+
+                :error ->
+                  raise "Incompatible dep!"
+              end
+
+            :error ->
+              {key, default_dep}
+          end
+        end
+    end
+  end
+
+  defp validate_dep(default_dep, new_dep) when is_atom(default_dep) and not is_atom(new_dep) do
+    :error
+  end
+
+  defp validate_dep(default_dep, new_dep) when is_atom(default_dep) and is_atom(new_dep) do
+    :ok
+  end
+
+  defp validate_dep(default_dep, new_dep) when is_function(default_dep) and not is_function(new_dep) do
+    :error
+  end
+
+  defp validate_dep(default_dep, new_dep) when is_function(default_dep) and is_function(new_dep) do
+    if :erlang.fun_info(default_dep)[:arity] == :erlang.fun_info(new_dep)[:arity] do
+      :ok
+    else
+      :error
+    end
+  end
+
+  defp validate_dep(_default_dep, _new_dep) do
+    :ok
   end
 end
--- test/diin_test.exs
+++ test/diin_test.exs
@@ -2,7 +2,8 @@ defmodule DiinTest do
   use ExUnit.Case
   doctest Diin
 
-  test "greets the world" do
-    assert Diin.hello() == :world
+  test "works" do
+    assert Diin.parse([deps: %{a: 2}, deps: %{a: 3}], %{a: 1}) == %{a: 2}
+    assert Diin.parse([], %{a: 1}) == %{a: 1}
   end
 end
